2faf2ff1db9be461663ba03bc795d2386afa82f3
HINKOKO <piotr62@live.fr>
2024-11-26 14:05:54 +0100

    final clean up on cob nums in test files

---
+++ APIX/SOURCE/COBTESTS/msgasser.cbl
@@ -0,0 +1,30 @@
+       IDENTIFICATION DIVISION.                                               
+       PROGRAM-ID. MSGASSER.                                                  
+                                                                              
+       DATA DIVISION.                                                         
+       WORKING-STORAGE SECTION.                                               
+       LINKAGE SECTION.                                                       
+       COPY TESTCONT.                                                         
+                                                                              
+       01 TEST-NAME PIC X(30).                                                
+       01 EXPECTED PIC  X(20).                                                
+       01 ACTUAL   PIC  X(20).                                                
+                                                                              
+       PROCEDURE DIVISION USING TEST-CONTEXT, TEST-NAME,                      
+                                       EXPECTED, ACTUAL.                      
+                                                                              
+           ADD 1 TO TESTS-RUN                                                 
+           IF ACTUAL EQUAL TO EXPECTED THEN                                   
+                     DISPLAY 'SUCCESS : ' TEST-NAME                           
+                     DISPLAY 'EXPECTED '  EXPECTED                            
+                     DISPLAY 'ACTUAL : '  ACTUAL                              
+                     ADD 1 TO PASSES                                          
+           ELSE                                                               
+                     DISPLAY 'FAILED : ' TEST-NAME                            
+                     DISPLAY 'EXPECTED ' EXPECTED                             
+                     DISPLAY 'ACTUAL : ' ACTUAL                               
+                     ADD 1 TO FAILURES                                        
+           END-IF                                                             
+                                                                              
+           GOBACK.                                                            
+       
