dbef958019ff44dbb2a09f124722c3a318a8134b
HINKOKO <piotr62@live.fr>
2024-11-26 14:07:12 +0100

    fix file extension

--- APIX/SOURCE/COBTESTS/asserter.cob
+++ APIX/SOURCE/COBTESTS/asserter.cob
@@ -1,28 +1,29 @@
-000100 IDENTIFICATION DIVISION.
-000200 PROGRAM-ID. ASSERTER.
-000300
-000400 DATA DIVISION.
-000500 WORKING-STORAGE SECTION.
-000600 LINKAGE SECTION.
-000700 COPY TESTCONT.
-000800
-000900 01 TEST-NAME PIC X(30).
-001000 01 EXPECTED PIC 9(3)V99.
-001100 01 ACTUAL   PIC 9(3)V99.
-001200                                                                        00
-001210 PROCEDURE DIVISION USING TEST-CONTEXT, TEST-NAME,                      00
-001220                          EXPECTED, ACTUAL.                             00
-001230                                                                        00
-001240     ADD 1 TO TESTS-RUN
-001250     IF ACTUAL = EXPECTED THEN
-001260               DISPLAY 'SUCCESS : ' TEST-NAME
-001270               ADD 1 TO PASSES
-001280     ELSE
-001290               DISPLAY 'FAILED : ' TEST-NAME
-001291               DISPLAY 'EXPECTED ' EXPECTED
-001292               DISPLAY 'ACTUAL : ' ACTUAL
-001293
-001294               ADD 1 TO FAILURES
-001295     END-IF
-001296                                                                        00
-001297     GOBACK.                                                            00
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. ASSERTER.
+       
+       DATA DIVISION.
+       WORKING-STORAGE SECTION.
+       LINKAGE SECTION.
+       COPY TESTCONT.
+       
+       01 TEST-NAME PIC X(30).
+       01 EXPECTED PIC 9(3)V99.
+       01 ACTUAL   PIC 9(3)V99.
+                                                                             
+       PROCEDURE DIVISION USING TEST-CONTEXT, TEST-NAME,                     
+                                EXPECTED, ACTUAL.                            
+                                                                             
+           ADD 1 TO TESTS-RUN
+           IF ACTUAL = EXPECTED THEN
+                     DISPLAY 'SUCCESS : ' TEST-NAME
+                     ADD 1 TO PASSES
+           ELSE
+                     DISPLAY 'FAILED : ' TEST-NAME
+                     DISPLAY 'EXPECTED ' EXPECTED
+                     DISPLAY 'ACTUAL : ' ACTUAL
+       
+                     ADD 1 TO FAILURES
+           END-IF
+                                                                             
+           GOBACK.                                                           
+       
--- APIX/SOURCE/COBTESTS/msgasser.cob
+++ APIX/SOURCE/COBTESTS/msgasser.cob
@@ -1,30 +1,30 @@
-000100 IDENTIFICATION DIVISION.                                               00
-000200 PROGRAM-ID. MSGASSER.                                                  00
-000300                                                                        00
-000400 DATA DIVISION.                                                         00
-000500 WORKING-STORAGE SECTION.                                               00
-000600 LINKAGE SECTION.                                                       00
-000700 COPY TESTCONT.                                                         00
-000800                                                                        00
-000900 01 TEST-NAME PIC X(30).                                                00
-001000 01 EXPECTED PIC  X(13).                                                00
-001100 01 ACTUAL   PIC  X(13).                                                00
-001200                                                                        00
-001300 PROCEDURE DIVISION USING TEST-CONTEXT, TEST-NAME,                      00
-001310                                 EXPECTED, ACTUAL.                      00
-001320                                                                        00
-001330     ADD 1 TO TESTS-RUN                                                 00
-001340     IF ACTUAL EQUAL TO EXPECTED THEN                                   00
-001350               DISPLAY 'SUCCESS : ' TEST-NAME                           00
-001360               DISPLAY 'EXPECTED '  EXPECTED                            00
-001370               DISPLAY 'ACTUAL : '  ACTUAL                              00
-001380               ADD 1 TO PASSES                                          00
-001390     ELSE                                                               00
-001391               DISPLAY 'FAILED : ' TEST-NAME                            00
-001400               DISPLAY 'EXPECTED ' EXPECTED                             00
-001500               DISPLAY 'ACTUAL : ' ACTUAL                               00
-001600                                                                        00
-001700               ADD 1 TO FAILURES                                        00
-001800     END-IF                                                             00
-001900                                                                        00
-002000     GOBACK.                                                            00
+       IDENTIFICATION DIVISION.                                               
+       PROGRAM-ID. MSGASSER.                                                  
+                                                                              
+       DATA DIVISION.                                                         
+       WORKING-STORAGE SECTION.                                               
+       LINKAGE SECTION.                                                       
+       COPY TESTCONT.                                                         
+                                                                              
+       01 TEST-NAME PIC X(30).                                                
+       01 EXPECTED PIC  X(20).                                                
+       01 ACTUAL   PIC  X(20).                                                
+                                                                              
+       PROCEDURE DIVISION USING TEST-CONTEXT, TEST-NAME,                      
+                                       EXPECTED, ACTUAL.                      
+                                                                              
+           ADD 1 TO TESTS-RUN                                                 
+           IF ACTUAL EQUAL TO EXPECTED THEN                                   
+                     DISPLAY 'SUCCESS : ' TEST-NAME                           
+                     DISPLAY 'EXPECTED '  EXPECTED                            
+                     DISPLAY 'ACTUAL : '  ACTUAL                              
+                     ADD 1 TO PASSES                                          
+           ELSE                                                               
+                     DISPLAY 'FAILED : ' TEST-NAME                            
+                     DISPLAY 'EXPECTED ' EXPECTED                             
+                     DISPLAY 'ACTUAL : ' ACTUAL                               
+                     ADD 1 TO FAILURES                                        
+           END-IF                                                             
+                                                                              
+           GOBACK.                                                            
+       
